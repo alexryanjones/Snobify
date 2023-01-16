3bd4aeb987f176d7ccf55ae86112e4ec0f50b29b
alexryanjones <alexryanjones@gmx.com>
2023-01-16 15:58:25 +0100

    db variables moved to env

--- server/database.js
+++ server/database.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
-const DB_PORT = process.env.DB_PORT || 27017;
-const DB_NAME = process.env.DB_NAME || 'snobify';
+// const DB_PORT = process.env.DB_PORT || 27017;
+// const DB_NAME = process.env.DB_NAME || 'snobify';
+
+const DB_PORT = process.env.DB_PORT;
+const DB_NAME = process.env.DB_NAME;
+
+
+console.log(process.env.DB_PORT);
+console.log(process.env.DB_NAME);
 
 
 mongoose.connect(
