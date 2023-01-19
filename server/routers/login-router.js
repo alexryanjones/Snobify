6d9ead7b6f8dcebcf78009dc3fd96ba01f3358e2
alexryanjones <alexryanjones@gmx.com>
2023-01-19 20:56:29 +0100

    health check added

--- server/routers/login-router.js
+++ server/routers/login-router.js
@@ -5,6 +5,7 @@ const login = require('../controllers/login.js');
 
 router.post('/login', login.Login)
 router.post('/refresh', login.Refresh);
+router.get('/health-check', (req, res) => res.status(200).send('Yes paaaaaal'))
 
 
 module.exports = router;
