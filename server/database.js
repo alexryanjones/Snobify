b9900b46485834da4ccb842ee820611f73e71335
alexryanjones <alexryanjones@gmx.com>
2023-01-19 21:46:07 +0100

    db port updated

--- server/database.js
+++ server/database.js
@@ -1,19 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const DB_PORT = process.env.DB_PORT;
-const DB_NAME = process.env.DB_NAME;
-// const MONGO_USERNAME = "doadmin";
-// const MONGO_PASSWORD = '';
-// const MONGO_HOSTNAME = "mongodb+srv://snobify-mongo-db-de3f4640.mongo.ondigitalocean.com"
-// const MONGO_DB = 'snobify';
-// const MONGO_PORT = 27017;
-
-// const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
-
 
 mongoose.connect(
-  // `mongodb://localhost:${process.env.DB_PORT}/${DB_NAME}`,
   process.env.DB_URL,
 
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -21,7 +10,7 @@ mongoose.connect(
     if (err) {
       console.log(`Sorry, something went wrong! ${err}`);
     } else {
-      console.log(`Database connected @ port ${DB_PORT}!`);
+      console.log(`Database connected @ port ${process.env.DB_PORT}!`);
     }
   }
 );
